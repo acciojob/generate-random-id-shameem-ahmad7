1eb928860e965ab7fab8be89d695c11db1070b0b
Shameem Ahmad <shameem.ahmad7714@gmail.com>
2024-09-26 21:17:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function makeid(l) {
   // write your code here
+	 // Define the characters to use for the random string (alphanumeric characters)
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  let result = '';
+
+  // Loop to generate a string of length l
+  for (let i = 0; i < l; i++) {
+    // Select a random character from the characters string
+    const randomIndex = Math.floor(Math.random() * charactersLength);
+    result += characters[randomIndex];
+  }
+
+  return result;
 }
 
 // Do not change the code below.
